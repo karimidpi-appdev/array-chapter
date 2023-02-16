ba263d666a5e5bc6a387e931ff090caef05a04f1
Mohammad Ali Karimi <ali216k@gmail.com>
2023-02-16 21:45:16 +0000

    Working on some methods of the Array class

--- array_element_square.rb
+++ array_element_square.rb
@@ -11,3 +11,8 @@ p "Enter at least 2 numbers, separated by spaces:"
 user_string = gets.chomp
 
 user_numbers = user_string.split
+
+second_to_last = user_numbers[-2].to_f
+square_num = second_to_last ** 2
+
+p square_num 
